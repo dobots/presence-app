0a3b07b1f6dce87444869f533117109f0952fd69
Dominik Egger <dominik@almende.org>
2015-08-12 16:09:53 +0200

    adjusted file header comments

--- app/src/main/java/nl/dobots/presence/cfg/Config.java
+++ app/src/main/java/nl/dobots/presence/cfg/Config.java
@@ -1,7 +1,21 @@
 package nl.dobots.presence.cfg;
 
 /**
- * Created by dominik on 6-8-15.
+ * Copyright (c) 2015 Dominik Egger <dominik@dobots.nl>. All rights reserved.
+ * <p/>
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as
+ * published by the Free Software Foundation.
+ * <p/>
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 3 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ * <p/>
+ * Created on 6-8-15
+ *
+ * @author Dominik Egger
  */
 public class Config {
 
--- app/src/main/java/nl/dobots/presence/gui/LocationsListActivity.java
+++ app/src/main/java/nl/dobots/presence/gui/LocationsListActivity.java
@@ -29,7 +29,21 @@ import nl.dobots.presence.locations.LocationsList;
 import nl.dobots.presence.utils.Utils;
 
 /**
- * Created by dominik on 6-8-15.
+ * Copyright (c) 2015 Dominik Egger <dominik@dobots.nl>. All rights reserved.
+ * <p/>
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as
+ * published by the Free Software Foundation.
+ * <p/>
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 3 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ * <p/>
+ * Created on 6-8-15
+ *
+ * @author Dominik Egger
  */
 public class LocationsListActivity extends ActionBarActivity {
 
